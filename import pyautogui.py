34f98e62a8a5fa7afd9752f98077d1614fa9921b
Samittnch <165297489+Samittnch@users.noreply.github.com>
2024-05-01 23:01:18 +0330

    Spam.py

--- import pyautogui.py
+++ import pyautogui.py
@@ -1,14 +1,5 @@
-import requests
-
-def get_weather_report(city_name):
-    url = f'https://wttr.in/{city_name}'
-    try:
-        response = requests.get(url)
-        # Print the text from the response
-        print(response.text)
-    except Exception as e:
-        print(f"An error occurred: {e}")
-
-# Replace 'City_Name' with the actual city name you want the weather report for
-city_name = input("Enter the name of the City: ")
-get_weather_report(city_name)
+import pyautogui as p, time
+time.sleep(5)
+for i in range(num of meesages):
+    p.typewrite("your message")
+    p.press("enter")
